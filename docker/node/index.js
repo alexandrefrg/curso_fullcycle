a0c6f1250d833cc122a828cff0deae31ff1e7031
Alexandre <alexandrefrg@hotmail.com>
2022-08-25 20:46:12 -0300

    feature: enhance app to connect and insert row in db

--- docker/node/index.js
+++ docker/node/index.js
@@ -1,11 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+const config = {
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "nodedb",
+};
 
-app.get('/', (req,res) => {
-  res.send('<h1>Full Cycle</h1>')
-})
+const mysql = require("mysql");
+const connection = mysql.createConnection(config);
+
+const sql = `INSERT INTO people(name) values ('Alexandre')`;
+connection.query(sql);
+connection.end;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Full Cycle</h1>");
+});
 
 app.listen(port, () => {
-  console.log('Rodando na porta ' + port)
-})
+  console.log("Rodando na porta " + port);
+});
