0f28f01feeb4676522b07872da3fd34948f2f64b
Alexandre <alexandrefrg@hotmail.com>
2022-09-03 02:04:16 -0300

    feat: including client side stream

--- modulo_comunicacao_sistemas/gRPC/cmd/client/client.go
+++ modulo_comunicacao_sistemas/gRPC/cmd/client/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"time"
 
 	"github.com/alexandrefrg/curso_fullcycle/pb"
 	"google.golang.org/grpc"
@@ -19,7 +20,8 @@ func main() {
 
 	client := pb.NewUserServiceClient(connection)
 	// AddUser(client)
-	AddUserVerbose(client)
+	// AddUserVerbose(client)
+	AddUsers(client)
 }
 
 func AddUser(client pb.UserServiceClient) {
@@ -60,4 +62,56 @@ func AddUserVerbose(client pb.UserServiceClient) {
 		}
 		fmt.Println("Status:", stream.Status, " - ", stream.GetUser())
 	}
+}
+
+func AddUsers(client pb.UserServiceClient) {
+	reqs := []*pb.User{
+		&pb.User{
+			Id:    "a1",
+			Name:  "Alexandre primeiro",
+			Email: "alexandre@primeiro.com",
+		},
+		&pb.User{
+			Id:    "a2",
+			Name:  "Alexandre segundo",
+			Email: "alexandre@segundo.com",
+		},
+		&pb.User{
+			Id:    "a3",
+			Name:  "Alexandre terceiro",
+			Email: "alexandre@terceiro.com",
+		},
+		&pb.User{
+			Id:    "z1",
+			Name:  "zezinho primeiro",
+			Email: "zezinho@primeiro.com",
+		},
+		&pb.User{
+			Id:    "z2",
+			Name:  "zezinho segundo",
+			Email: "zezinho@segundo.com",
+		},
+		&pb.User{
+			Id:    "z3",
+			Name:  "zezinho terceiro",
+			Email: "zezinho@terceiro.com",
+		},
+	}
+
+	stream, err := client.AddUsers(context.Background())
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
+
+	for _, req := range reqs {
+		stream.Send(req)
+		time.Sleep(time.Second * 3)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("Error receiving response: %v", err)
+	}
+
+	fmt.Println(res)
 }
